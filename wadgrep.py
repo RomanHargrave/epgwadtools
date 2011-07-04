e4412de679690812e54587a8a895993bf4ddd5dd
epg <epg@pretzelnet.org>
2011-07-04 12:45:07 -0700

    only checkk up/mid if two_sided to eliminate false positives

--- wadgrep.py
+++ wadgrep.py
@@ -7,22 +7,27 @@ import omg
 
 def grep(pattern, map_editor):
     search = re.compile(pattern, re.IGNORECASE).search
-    line_map = map_sidedefs_to_linedefs(map_editor)
+    linedefs = map_editor.linedefs
+    line_map = map_sidedefs_to_linedefs(linedefs)
     found = False
     for i, sidedef in enumerate(map_editor.sidedefs):
-        if (search(sidedef.tx_up)
-            or search(sidedef.tx_mid)
-            or search(sidedef.tx_low)):
+        linedef_num = line_map[i]
+        if search(sidedef.tx_mid):
             found = True
-            print 'sidedef', i, 'of linedef', line_map[i]
+            print 'sidedef', i, 'of linedef', linedef_num
+            continue
+        if linedefs[linedef_num].two_sided:
+            if search(sidedef.tx_mid) or search(sidedef.tx_low):
+                found = True
+                print 'sidedef', i, 'of linedef', linedef_num
     if found:
         return 0
     # Traditionally 1, but Python uses that for uncaught exception.
     return 2
 
-def map_sidedefs_to_linedefs(map_editor):
+def map_sidedefs_to_linedefs(linedefs):
     result = {}
-    for i, linedef in enumerate(map_editor.linedefs):
+    for i, linedef in enumerate(linedefs):
         result[linedef.front] = i
         if linedef.two_sided:
             result[linedef.back] = i
